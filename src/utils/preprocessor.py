54c6b46df0938cb8474a7d9a7712fea3ef6e46e4
opeyemibami <bamigbadeopeyemi@gmail.com>
2020-11-27 00:19:23 +0100

    project structuring

---
+++ src/utils/preprocessor.py
@@ -0,0 +1,22 @@
+from . import feature_engineering as ft
+import pandas as pd
+
+num_cols = ['age', 'height', 'weight', 'ap_hi', 'ap_lo', 'bmi']
+multi_cols = ['cholesterol', 'gluc']
+
+def preprocess_data(dataframe,scaler,one_hot_enc):
+
+    data = ft.feature_eng(dataframe=dataframe)
+
+    #One hot encoding
+    hot_encoded_data = one_hot_enc.transform(data[multi_cols])
+    d = pd.DataFrame(data=hot_encoded_data,columns=['cholesterol_normal','cholesterol_above_normal','cholesterol_well_above_normal','glucose_normal','glucose_above_normal','glucose_well_above_normal'],dtype=int ).reset_index(drop=True)
+    data.drop(columns=multi_cols,axis=1,inplace=True)
+    data = pd.concat(objs=[data,d],axis=1)
+
+    #scale numerical columns
+    scaler = scaler
+    scaled = scaler.transform(data[num_cols])
+    data[num_cols] = scaled
+    return data
+
